a034785019e04e2826874f97ebb941e818f2ea99
Andrew Brennwald <leopawnz@gmail.com>
2015-08-28 16:16:40 -0700

    Implement band deletion to front-end.

--- app.rb
+++ app.rb
@@ -15,7 +15,18 @@ get("/bands") do
 end
 
 post("/bands") do
-  @band = Band.create({:name => params.fetch("name")})
+  @band = Band.create({:name => params.fetch("band_name")})
+  redirect back
+end
+
+get("/bands/:id") do
+  @band = Band.find(params.fetch("id").to_i())
+  erb(:band)
+end
+
+patch("/bands/:id") do
+  @band = Band.find(params.fetch("id").to_i())
+  @band.update({:name => params.fetch("update_band_name")})
   redirect back
 end
 
--- spec/band_pages_spec.rb
+++ spec/band_pages_spec.rb
@@ -4,8 +4,18 @@ describe("adding a band", {:type => :feature}) do
   it("allows the user to add a band to the database") do
     visit("/")
     click_link("Bands")
-    fill_in("name", :with => "Coheed and Cambria")
+    fill_in("band_name", :with => "Coheed and Cambria")
     click_button("Add Band")
     expect(page).to have_content("Coheed and Cambria")
+  end
+end
+
+describe("updating a band", {:type => :feature}) do
+  it("allows the user to update a band's name") do
+    test_band = Band.create({:name => "Coheed and Cambria"})
+    visit("/bands/#{test_band.id()}")
+    fill_in("update_band_name", :with => "The Prize Fighter Inferno")
+    click_button("Update Name")
+    expect(page).to have_content("The Prize Fighter Inferno")
   end
 end
